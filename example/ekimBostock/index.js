02f218535c5661424660f459b5592fccf3e79282
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2025-10-24 14:39:51 +0200

    Update index.js

--- example/ekimBostock/index.js
+++ example/ekimBostock/index.js
@@ -1,17 +1,18 @@
-const Graph = ForceGraph3D()(document.getElementById("3d-graph"));
+const Graph = ForceGraph3D()
+ (document.getElementById("3d-graph"));
+
 let curDataSetIdx;
 const dataSets = getGraphDataSets();
+
 let toggleData;
-(toggleData = function() 
- {
+(toggleData = function()  {
         curDataSetIdx = curDataSetIdx === undefined ? 0 : (curDataSetIdx + 1) % dataSets.length;
         const dataSet = dataSets[curDataSetIdx];
-        Graph.resetProps(); 
-   // 
-   Wipe current state dataSet(Graph); 
-   // 
-   Load data set document.getElementById('graph-data-description').innerHTML = dataSet.description ? `Viewing ${
-     dataSet.description}` : ''; 
- })(); 
+ 
+        Graph.resetProps();    //    Wipe current state 
+  dataSet(Graph);    //    Load data set 
+ 
+  document.getElementById('graph-data-description').innerHTML = dataSet.description ? `Viewing ${dataSet.description}` : ''; 
+ })(); // IIFE init
 
-// IIFE init
+
